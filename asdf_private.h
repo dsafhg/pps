a9345c3078780023c86b8c088dc5416f876e11e3
Dimitar <dsafhg@users.noreply.github.com>
2017-05-05 16:35:29 +0300

    task1

---
+++ asdf_private.h
@@ -0,0 +1,25 @@
+/*
+ * File: asdf_private.h
+ *
+ * Code generated for Simulink model 'asdf'.
+ *
+ * Model version                  : 1.0
+ * Simulink Coder version         : 8.11 (R2016b) 25-Aug-2016
+ * C/C++ source code generated on : Thu Mar 16 17:08:21 2017
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_asdf_private_h_
+#define RTW_HEADER_asdf_private_h_
+#include "rtwtypes.h"
+#endif                                 /* RTW_HEADER_asdf_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
